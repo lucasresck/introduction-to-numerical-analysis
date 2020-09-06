6dc3120625af20c36cc95c361c3b4e22ede95e39
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2020-09-05 23:27:17 -0300

    Update function

--- list_0/edo.m
+++ list_0/edo.m
@@ -1,10 +1,15 @@
-function x_1 = edo(x_0, N)
-    x_1 = zeros(N, 1);
-    x_1(1) = x_0;
-    for n=2:N
-        x_1(n) = x_0;
+function x = edo(x_0, tol)
+    x = [x_0];
+    diff = 10^3;
+    n = 2;
+    while diff > tol
+        x = [x, x_0];
         for i=1:(n-1)
-           x_1(n) = x_1(n)*(1+i/(n*n));
+           x(n) = x(n)*(1+i/(n*n));
         end
+        diff = x(n) - x(n-1);
+        n = n+1;
     end
+    x = -x + x_0*sqrt(exp(1));
+    plot(x);
 end
