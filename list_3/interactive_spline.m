a496ea636736c318b94fcde25df96e6c7eec0f75
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2020-10-23 13:43:41 -0300

    Improve plot of exercise 12

--- list_3/interactive_spline.m
+++ list_3/interactive_spline.m
@@ -1,17 +1,35 @@
-function [x, y] = interactive_spline(n, k, k_0, k_1)
+function interactive_spline(n, k, k_0, k_1)
     clf;
     axis([0, 10, 0, 10]);
     hold on;
     [x_click, y_click] = ginput(1);
     x = [x_click];
     y = [y_click];
-    plot(x, y, 'o');
+    plot(x, y, 'o', ...
+        'MarkerEdgeColor', 'k', ...
+        'MarkerFaceColor', 'k');
     for j = 1:k
         for i = 1:n+2
             [x_click, y_click] = ginput(1);
             x = [x; x_click];
             y = [y; y_click];
-            plot(x, y, 'o');
+            if i == 1 || i == n+2
+                plot(x_click, y_click, 'o', ...
+                'MarkerEdgeColor', 'r', ...
+                'MarkerFaceColor', 'r');
+                plot(x(end-1:end), y(end-1:end), '--', ...
+                    'Color', [200, 200, 200]/255);
+            else
+                if i == n+1
+                    plot(x_click, y_click, 'o', ...
+                    'MarkerEdgeColor', 'k', ...
+                    'MarkerFaceColor', 'k');
+                else
+                    plot(x_click, y_click, 'o', ...
+                    'MarkerEdgeColor', 'b', ...
+                    'MarkerFaceColor', 'b');
+                end
+            end
         end
         parametric_cubic_spline(x, y, k_0, k_1);
         x = [x(end-1)];
--- list_3/parametric_cubic_spline.m
+++ list_3/parametric_cubic_spline.m
@@ -1,5 +1,5 @@
 function parametric_cubic_spline(x, y, k_0, k_1)
     x_t = anchored_cubic_spline(x, k_0, k_1);
     y_t = anchored_cubic_spline(y, k_0, k_1);
-    plot(x_t, y_t);
+    plot(x_t, y_t, '-k');
 end
